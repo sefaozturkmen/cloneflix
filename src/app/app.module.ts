a6fa42f67149b1b1ef63a840faf57da6ea013555
sefaozturkmen <sipilett@gmail.com>
2021-02-13 02:29:26 +0300

    feat: Created Pages and İnstalled Package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SwiperModule } from 'swiper/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstpageComponent } from './pages/firstpage/firstpage.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstpageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/firstpage/firstpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstpage',
+  templateUrl: './firstpage.component.html',
+  styleUrls: ['./firstpage.component.scss']
+})
+export class FirstpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
