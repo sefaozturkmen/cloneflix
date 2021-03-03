c689197be3104e16b1b2fe11e8588f5971047140
sefaozturkmen <sipilett@gmail.com>
2021-03-03 14:03:35 +0300

    created and designed footer component

---
+++ src/app/datasources/dev-list.ts
@@ -0,0 +1,7 @@
+import { Dev } from '../models'
+
+
+export const DevList:Dev[]=[
+    {image:'girl.svg',name:'Zeynep İşbay', mail:'zeynepisbay@gmail.com',github:'https://github.com/zeynepisbay',linkedin:'https://www.linkedin.com/in/zeynepisbay'},
+    {image:'boy.svg',name:'Sefa Öztürkmen', mail:'sefaozturkmenn@gmail.com',github:'https://github.com/sefaozturkmen',linkedin:'https://www.linkedin.com/in/sefaozturkmen'}
+]
--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,3 +1,4 @@
 import { from } from "rxjs";
 
-export *from './user'
+export * from './user'
+export *from './dev'
